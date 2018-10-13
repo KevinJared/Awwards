741bb8be714e1b2c69cbfd9066ef8d75d1e38368
kevinjared <kevinjaredkip@gmail.com>
2018-10-13 17:38:33 +0300

    update of views

--- awwardsmain/views.py
+++ awwardsmain/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import redirect
+from awwardsmain.models import Post, Profile
+from django.contrib.auth.models import User
+from .forms import NewPostForm, UserForm, ProfileForm,CommentForm
+from django.contrib.auth.decorators import login_required
+import datetime as dt
 
-# Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    current_user = request.user.id
+    user = request.user
+    date = dt.date.today()
+    posts = Post.objects.all()
+    if Profile.objects.filter(user = request.user).count() == 0:
+        prof = Profile(user=request.user)
+        prof.save()
+    return render(request, 'index.html',{"date": date, "posts": posts})
+
+@login_required(login_url='/accounts/login/')
+def profile(request,user_id=None):
+    if user_id == None:
+        user_id=request.user.id
+    current_user = User.objects.get(id = user_id)
+    user = current_user
+    images = Post.objects.filter(user=current_user)
+    profile = Profile.objects.all()
+    return render(request, 'profile.html', locals())
+
+@login_required(login_url='/accounts/login')
+def updateprofile(request):
+	if request.method == 'POST':
+		form = ProfileForm(request.POST,request.FILES, instance=request.user.profile)
+		if form.is_valid():
+			form.save()
+			return redirect('profile')
+
+	else:
+			form = ProfileForm()
+	return render(request, 'updateprofile.html',{"form":form })
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+            return redirect('index')
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form":form})
